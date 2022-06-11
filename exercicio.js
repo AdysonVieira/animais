759053b8b094cab7f49a6db41b2ec7a26ae9dcab
Adyson Vieira <contato.adyson@gmail.com>
2022-06-11 14:53:38 -0300

    exercicio 2

--- exercicio.js
+++ exercicio.js
@@ -1,4 +1,4 @@
-// Exercício
+// Exercício 1
 
 // Retorne no console todas as imagens do site
 const imagensHTML = document.getElementsByTagName('img'); // Ao vivo
@@ -14,8 +14,36 @@ const linksInternos = document.querySelectorAll('a[href^="#"]');
 
 // Selecione o primeiro h2 dentro de .animais-info
 const animais = document.querySelector('.animais-info');
-const primeiroH2 = animais.querySelector('h2')
+const primeiroH2 = animais.querySelector('h2');
 
 // Selecione o último p do site
-const todosOsP = document.querySelectorAll('p')
-console.log(todosOsP[--todosOsP.length])
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos[--paragrafos.length]);
+
+
+// Exercício 2
+
+// Mostre no console cada parágrafo do site
+paragrafos.forEach(function(paragrafo) {
+    console.log(paragrafo);
+})
+
+paragrafos.forEach(paragrafo => console.log(paragrafo)); // Arrow function
+
+// Mostre o texto dos parágrafos no console
+paragrafos.forEach(paragrafo => console.log(paragrafo.innerText)); // Arrow function
+
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => { // Colocando o argumentos da função entre ()
+    console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+    console.log(i++);
+});
+
+imgs.forEach(() => i++);
