3896d8dd568445a8fbc4a575c845128b6fe424a0
Adyson Vieira <contato.adyson@gmail.com>
2022-10-10 16:25:37 -0300

    dropdown menu ok

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,16 +1,38 @@
 export default function initDropdown() {
     const dataDropdown = document.querySelectorAll('[data-dropdown]');
-    const dropdownMenu = document.querySelector('.dropdown-menu')
-
-    function addActiveClick(event) {
-        event.stopPropagation();
-        dropdownMenu.classList.toggle('active')
-    }
-
+    const dropdownMenu = document.querySelector('.menu-dropdown')
+    const html = document.documentElement;
+        
     dataDropdown.forEach(data => {
-        const events = ['click', 'touchstart']
-        data.addEventListener(events, addActiveClick, {
-            capture: false
+        ['click', 'touchstart'].forEach(userEvent => {
+            data.addEventListener(userEvent, addActiveClick, {
+                capture: false
+            })
         })
     })
+    
+    function addActiveClick(event) {
+        event.stopPropagation();
+        event.preventDefault(); // lembrar de remover essa linha quando adicionar links as páginas
+        dropdownMenu.classList.toggle('active');
+        // cria o evento no HTML se conter ativo
+        if(dropdownMenu.classList.contains('active')){
+            ['click', 'touchstart'].forEach(userEvent => {
+                html.addEventListener(userEvent, clickOut, {
+                    capture: false
+                })
+            })
+        }
+    }
+    
+    function clickOut() {
+        if(dropdownMenu.classList.contains('active')) {
+            dropdownMenu.classList.remove('active');
+            ['click', 'touchstart'].forEach(userEvent => {
+                html.removeEventListener(userEvent, clickOut, {
+                    capture: false
+                })
+            })
+        }
+    }
 }
