9253c7c9162ace50a93d6600cba3b650899d5a5d
Adyson Vieira <contato.adyson@gmail.com>
2022-12-19 17:08:34 -0300

    refatorando tabnav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import initTabNav from "./modules/tab-nav.js";
 import Accordion from "./modules/accordion.js";
+import TabNav from "./modules/tab-nav.js";
 import initScrollReveal from "./modules/scroll-reveal.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -10,9 +10,11 @@ import initAnimaisFetch from "./modules/animais-fetch.js";
 import initBitcoinFetch from "./modules/bitcoin-fetch.js";
 
 const accordion = new Accordion('[data-anime="accordion"] dt')
-accordion.init()
+accordion.init();
 
-initTabNav();
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] div')
+tabNav.init();
+
 initScrollReveal();
 initModal();
 initTooltip();
