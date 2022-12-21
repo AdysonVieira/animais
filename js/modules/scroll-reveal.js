c9e107877edf5213872cf34c301ffe6819d86089
Adyson Vieira <contato.adyson@gmail.com>
2022-12-21 12:36:29 -0300

    refatorando animação ao scroll

--- js/modules/scroll-reveal.js
+++ js/modules/scroll-reveal.js
@@ -3,11 +3,12 @@ export default class ScrollReveal {
         this.elements = document.querySelectorAll(elements);
         this.className = className
         
-        // bind
+        // bind para o this da class
         this.handleElements = this.handleElements.bind(this)
         this.constructor.animation = this.constructor.animation.bind(this)
     }
 
+    // constructor que recebe um elemento, verifica se o topo do elemento está na metade da tela e adiciona a class de animação
     static animation(element) {
         const elementTop = element.getBoundingClientRect().top;
         const windowHeight = window.innerHeight * 0.5;
@@ -17,14 +18,17 @@ export default class ScrollReveal {
         : ''
     }
 
+    // adiciona o evento de scroll ao window
     addScrollEvent() {
         window.addEventListener('scroll', this.handleElements)
     }
 
+    // percorre os elementos os elementos e 
     handleElements() {
         this.elements.forEach(( element ) => this.constructor.animation( element ))
     }
 
+    // inicia o objeto da class, verifica se contém elementos e inicia a chamada do evento
     init() {
         if (this.elements.length) {
             this.constructor.animation(this.elements[0])
